2b752caa4c14beb466eaf02ad45e6b02ad898de8
Abdul basith kp <basithdude85@gmail.com>
2026-09-25 15:43:05 +0530

    make landing page modern

--- static/js/index.js
+++ static/js/index.js
@@ -1,21 +1,2 @@
 
-const words = [
-        'Completed',
-        'Finished',
-        'Concluded',
-        'Accomplished',
-        'Fulfilled',
-        'Finalized',
-        'Achieved',
-        'Executed',
-        'Done'];
 
-const heroMainSpan = document.querySelector(".hero-main-span");
-
-let index = 0;
-
-setInterval(() => {
-    heroMainSpan.innerText = words[index];
-
-    index = (index + 1) % words.length;
-}, 1000);
